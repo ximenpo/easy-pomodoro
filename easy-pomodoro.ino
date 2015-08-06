0ac19602d78554fbd444afbf08d00cc7b91889af
ximenpo <ximenpo@jiandan.ren>
2015-08-06 21:48:56 +0800

    erase comment.

--- easy-pomodoro.ino
+++ easy-pomodoro.ino
@@ -4,12 +4,10 @@ extern  void  led_setflash(bool flash);
 extern  void  led_update(unsigned int value);
 
 void setup() {
-  // put your setup code here, to run once:
   led_init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   led_update(millis() / 1000);
 }
 
