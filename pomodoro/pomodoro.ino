92b529d905f88c0561344630dbc7a40e412263ab
ximenpo <ximenpo@jiandan.ren>
2015-08-12 11:24:52 +0800

    alert support.

--- pomodoro/pomodoro.ino
+++ pomodoro/pomodoro.ino
@@ -8,34 +8,60 @@ static  timestamp<>     timer_;
 static  debouncing<>    btn_;
 static  pomodoro_logic  logic_;
 
+static  timing<>        timing_confirm_;
+static  timing<>        timing_alert_;
+
 static  const int       pin_button  = 14;
+static  const int       pin_alert   = 12;
 
 void setup() {
   led_init();
   btn_.init(pin_button);
   logic_.init();
   timer_.reset(0);
+
+  pinMode(pin_alert, OUTPUT);
+  timing_confirm_.init();
+  timing_alert_.init();
+
+  timing_confirm_.done();
+  timing_alert_.done();
 }
 
 void loop() {
   // update logic with tow input: time and button
-  logic_.update(timer_.now(), btn_.check_click(timer_.now(), HIGH));
+  unsigned long timestamp = timer_.now();
+  logic_.update(timestamp, btn_.check_click(timestamp, HIGH));
 
   // display using logic result
-  /*
   if (logic_.confirming) {
+    static  bool  alert_status  = false;
     if (!led_isblink()) {
-      analogWriteFreq(262);
-      analogWrite(12, 1023);
+      // begin alert
+      if (logic_.remain_seconds == 0) {
+        alert_status  = true;
+        timing_alert_.reset();
+      } else {
+        timing_confirm_.reset();
+      }
+      digitalWrite(pin_alert, HIGH);
+    }
+    // check & stop alert
+    if (timing_alert_.check_periodic(timestamp, 500, 10)) {
+      alert_status  = !alert_status;
+      //digitalWrite(pin_alert, alert_status ? HIGH : LOW);
+    }
+    if (timing_confirm_.check_timeout(timestamp, 1000)) {
+      digitalWrite(pin_alert, LOW);
     }
   } else {
-    analogWrite(12, 0);
-  }*/
+    digitalWrite(pin_alert, LOW);
+  }
   led_setblink(logic_.confirming);
   led_update(logic_.remain_seconds);
 
   // next round
-  if (logic_.is_finished()) {
+  if (logic_.is_done()) {
     logic_.reset();
     timer_.reset();
   }
