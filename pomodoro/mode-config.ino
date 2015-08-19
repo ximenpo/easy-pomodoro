63c741945f23863f8e8ee7d4e21950d27c18f2ab
ximenpo <ximenpo@jiandan.ren>
2015-08-19 18:35:10 +0800

    split running&config mode.

---
+++ pomodoro/mode-config.ino
@@ -0,0 +1,18 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+
+static  const byte          DNS_PORT = 53;
+static  IPAddress          ap_ip_(192, 168, 1, 1);
+static  DNSServer           dns_;
+static  ESP8266WebServer  web_(80);
+
+void  config_setup() {
+
+}
+
+void  config_loop() {
+  dns_.processNextRequest();
+  web_.handleClient();
+}
+
--- pomodoro/pomodoro.ino
+++ pomodoro/pomodoro.ino
@@ -1,82 +1,28 @@
-#include  <timestamp.h>
-#include  <debouncing.h>
-#include  <timing.h>
-
+#include  <procedure.h>
 #include  "hw-define.h"
-#include  "logic.h"
-#include  "logic-config.h"
 
-static  timestamp<>     timer_;
-static  debouncing<>    btn_;
-static  pomodoro_logic  logic_;
-static  logic_config    config_;
-
-static  timing<>        timing_confirm_;
-static  timing<>        timing_alert_;
-
-static  bool      alerting    = false;
+static  bool  is_config_mode  = false;
+static  void  (*func_setup)() = 0;
+static  void  (*func_loop)() = 0;
 
 void setup() {
-  pinMode(pin_buzzer, OUTPUT);
-  digitalWrite(pin_buzzer, LOW);
+  unsigned  long  start = millis();
+  do {
+    if (digitalRead(pin_button) == HIGH) {
+      is_config_mode = true;
+      break;
+    }
+  } while (millis() - start < 500);
 
-  config_.init();
-  config_.load();
+  {
+    func_setup  = is_config_mode  ? config_setup  : running_setup;
+    func_loop   = is_config_mode  ? config_loop   : running_loop;
+  }
 
-  led_init();
-  btn_.init(pin_button);
-  logic_.init(config_);
-  timer_.reset(0);
-
-  timing_confirm_.init();
-  timing_alert_.init();
-
-  timing_confirm_.done();
-  timing_alert_.done();
+  func_setup();
 }
 
 void loop() {
-  // update logic with tow input: time and button
-  unsigned long timestamp = timer_.now();
-  logic_.update(timestamp, btn_.check_click(timestamp, HIGH));
-
-  // display using logic result
-  if (logic_.confirming) {
-    static  bool  alert_status  = false;
-    if (!led_isblink()) {
-      // begin alert
-      if (logic_.remain_seconds == 0) {
-        alert_status  = true;
-        timing_alert_.reset();
-      } else {
-        timing_confirm_.reset();
-      }
-      alerting  = true;
-      digitalWrite(pin_buzzer, HIGH);
-    }
-    // check & stop alert
-    if (timing_alert_.check_periodic(timestamp, 500, 11)) {
-      alert_status  = !alert_status;
-      digitalWrite(pin_buzzer, alert_status ? HIGH : LOW);
-    }
-    if (timing_confirm_.check_timeout(timestamp, 1000)) {
-      alerting  = false;
-      digitalWrite(pin_buzzer, LOW);
-    }
-  } else if (alerting) {
-    digitalWrite(pin_buzzer, LOW);
-    alerting  = false;
-    timing_confirm_.done();
-    timing_alert_.done();
-  }
-
-  led_setblink(logic_.confirming);
-  led_update(logic_.remain_seconds);
-
-  // next round
-  if (logic_.is_done()) {
-    logic_.reset();
-    timer_.reset();
-  }
+  func_loop();
 }
 
